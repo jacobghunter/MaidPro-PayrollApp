1169615b05dd11acdc26563f5678aea1331af5b7
jacobghunter <98200224+jacobghunter@users.noreply.github.com>
2023-03-20 22:38:19 -0700

    calculations and writing to file done
    tkinter error pop up also done

--- employee.py
+++ employee.py
@@ -1,24 +1,57 @@
-class employee:
+class employee:    
     def __init__(self, employee_name, code):
         self.employee_name = employee_name
         self.code = code
-        self.categories = {}
         # stored as ints
-        self.tip = 0
-        self.reimbursement = 0
-    def update_hours(self, category, hours):
-        if category in self.categories.keys():
-            self.categories[category] += hours
-        else:
-            self.categories[category] = hours
-    def update_tip(self, tip):
-        self.tip += tip
-    def update_reimbursement(self, reimbursement):
-        self.reimbursement += reimbursement
-    def final_output(self):
-        # subtract overtime from 'regular' hours
-        # convert back to floats
+
+        self.regular = ["cleaner", "manager"]
+
+        self.hours = {"regular": 0, 
+                    "dispatch": 0, 
+                    "overtime": 0,
+                    "office work": 0,
+                    "travel": 0,
+                    "trainee": 0,
+                    "trainer": 0,
+                    "reimbursement": 0,
+                    "paycheck tips": 0,
+                    "qc": 0,
+                    "vacation": 0,
+                    "holiday": 0,
+                    "bonus": 0,
+                    "paid time off": 0}
         
-        pass
+    def update_hours(self, category, time):
+        if str(time) == 'nan':
+            return
+        for item in self.hours.keys():
+            if item in category.lower():
+                self.hours[item] += time
+                return
+        if category.lower() in self.regular:
+            self.hours["regular"] += time
+            return
+        print(category.lower())
+        print(self.hours.keys())
+        print("MISSING HOUR CLASSIFICATIONS")
+        exit(1)
+
+    def update_tip(self, tip):
+        if str(tip) == 'nan':
+            return
+        self.hours["paycheck tips"] += tip
+
+    def update_reimbursement(self, reimbursement):
+        if str(reimbursement) == 'nan':
+            return
+        self.hours["reimbursement"] += reimbursement
+
+    def final_hours(self):
+        self.hours["regular"] -= self.hours["overtime"]
+        # for if they have no regular hours
+        if self.hours["regular"] < 0:
+            self.hours["regular"] = 0
+        return [self.code] + [item / 100 for item in self.hours.values()]
+
     def __str__(self):
-        return self.employee_name + " " + self.code + " " + str(self.tip / 100) + " " + str(self.reimbursement / 100) + ' ' + str(self.categories)
+        return self.employee_name + " " + self.code + " " + str(self.tip / 100) + " " + str(self.reimbursement / 100) + ' ' + str(self.hours)
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import tkinter
 from tkinter import filedialog
+from tkinter import messagebox
 from openpyxl import load_workbook
 
 import datetime
@@ -13,18 +14,14 @@ if __name__ == "__main__":
     EMPLOYEE_CODES = 'Employee Codes'
 
     tkinter.Tk().withdraw() # prevents an empty tkinter window from appearing
-    # file = filedialog.askopenfile(mode='r')
     file_path = filedialog.askopenfilename(filetypes=[("Excel files", ".xlsx .xls")])
 
     wb = load_workbook(file_path)
 
     codesFile = pd.read_excel(file_path, sheet_name=EMPLOYEE_CODES)
-    dataFile = pd.read_excel(file_path)
-
     tempCodes = np.asarray(codesFile)
-    tempData = np.asarray(dataFile)
 
-    # column by column
+    # reading in employee codes
     codes = []
     for item in tempCodes:
         tempList = []
@@ -34,6 +31,10 @@ if __name__ == "__main__":
         if len(tempList) != 0:
             codes.append(tempList)
 
+    dataFile = pd.read_excel(file_path)
+    tempData = np.asarray(dataFile)
+
+    # reading in data
     data = []
     for item in tempData:
         tempList = []
@@ -42,41 +43,41 @@ if __name__ == "__main__":
             for item2 in item:
                 # converting to int to avoid weird errors
                 if isinstance(item2, float) and str(item2) != 'nan':
-                    tempList.append(int(item2 * 100))
+                    tempList.append(round(item2 * 100))
                 else:
                     tempList.append(item2)
         if len(tempList) != 0:
             data.append(tempList)
 
-    # 7 = hours of entry
 
-    # writer = pd.ExcelWriter(file_path, engine='openpyxl')
-
-    # 9=tip, 10=adjustment, 11=adjreason, 13=name, 14=category
-
+    # employee creation
     employees = {}
     for code in codes:
         if str(code[0]) != 'nan': 
             name = code[0].lower() + ' ' + code[1].lower()
             employees[name] = employee(name, code[2])
 
-    print(data)
+    missing_names = set()
+
+    # updating employees with data
     for entry in data:
         # update hours with the category and the hours
-        # print(entry[13], entry[12])
         if entry[13].lower() not in employees.keys():
-            # tkinter pop up
-            pass
-            # exit(1)
+            # add missing names to set
+            missing_names.add(entry[13])
         else:
             employees[entry[13].lower()].update_hours(entry[14], entry[7])
             employees[entry[13].lower()].update_tip(entry[9])
             employees[entry[13].lower()].update_reimbursement(entry[10])
-    
-    # print(employees.values())
-    for person in employees.values():
-        print(person)
 
-# https://pandas.pydata.org/docs/reference/api/pandas.read_excel.html
-# https://thinkingneuron.com/add-new-sheet-to-excel-using-pandas/
-# https://stackoverflow.com/questions/66663179/how-to-use-windows-file-explorer-to-select-and-return-a-directory-using-python
+    # add pop up for all missing names
+    if len(missing_names) > 0:
+        messagebox.showerror("Python Error", f"Missing employee code for {missing_names}")
+        exit(1)
+
+    # performing final calculation and outputting
+    final_output = []
+    for person in employees.values():
+        final_output.append(person.final_hours())
+    
+    np.savetxt("output.csv", final_output, delimiter=", ",fmt='% s')
